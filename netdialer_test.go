785dc615db649263f8c3f719bfef313ea121a764
pa001024 <pa001024@qq.com>
2015-09-30 10:57:54 +0800

    remove test

--- netdialer_test.go
+++ netdialer_test.go
@@ -16,26 +16,7 @@ import (
 var dialer *Dialer
 
 func init() {
-	dialer = NewDialer("17805805321@HYXY.XY", "266174")
-}
-
-type TEMPSTR struct {
-	UserIP   string `xml:"Redirect>UserIP"`
-	LoginURL string `xml:"Redirect>LoginURL"`
-	Uuid     string `xml:"Redirect>Uuid"`
-}
-
-func TestStep1(t *t.T) {
-	par := url.Values{"wlanuserip": {"10.0.1.2"}}
-	body := ioutil.NopCloser(strings.NewReader(par.Encode()))
-	req, _ := http.NewRequest("POST", "http://115.239.134.163:8080/showlogin.do", body)
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; ")
-	req.Header.Set("User-Agent", "China Telecom Client")
-	res, _ := http.DefaultClient.Do(req)
-	de := xml.NewDecoder(res.Body)
-	v := &TEMPSTR{}
-	de.Decode(v)
-	fmt.Println(v)
+	dialer = NewDialer("17805805321@HYXY.XY", "123456")
 }
 
 func TestGetCryptUsername(t *t.T) {
