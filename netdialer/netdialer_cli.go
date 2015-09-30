83767997dacb9bb3552be23dbb705d5627589dda
pa001024 <pa001024@qq.com>
2015-09-30 22:02:48 +0800

    update cli

--- netdialer/netdialer_cli.go
+++ netdialer/netdialer_cli.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"time"
 
 	"github.com/pa001024/netdialer"
@@ -12,11 +11,17 @@ import (
 func main() {
 	username := flag.String("u", "", "shanxun username")
 	password := flag.String("p", "", "shanxun password")
-	isRouter := flag.Bool("r", true, "use router?")
+	isRouter := flag.Bool("r", false, "use router?")
 	routerIP := flag.String("ra", "192.168.1.1", "router IP")
 	routerUser := flag.String("ru", "admin", "router User")
 	routerPwd := flag.String("rp", "admin", "router password")
+	noCheck := flag.Bool("nc", false, "no check:dont need check?")
 	flag.Parse()
+	if len(*username) < 12 || len(*password) != 6 {
+		util.ERROR.Log("Invalid username or password")
+		flag.Usage()
+		return
+	}
 	d := netdialer.NewDialer(*username, *password)
 	var err error
 	if *isRouter {
@@ -24,22 +29,24 @@ func main() {
 		d.Router.User = *routerUser
 		d.Router.Pwd = *routerPwd
 		err = d.ConnectRouter()
-		check()
+		if !*noCheck {
+			check()
+		}
 	} else {
 		err = d.ConnectDirect()
-		check()
+		if !*noCheck {
+			check()
+		}
 	}
 	if err != nil {
-		fmt.Errorf("%s", err)
+		util.ERROR.Log(err)
 	}
 }
 
 func check() {
-	go func() {
-		<-time.After(time.Second * 2)
-		ip := util.GetIPInfo()
-		if ip != nil {
-			util.INFO.Log("连接成功, 当前IP:", ip.IP)
-		}
-	}()
+	<-time.After(time.Second * 2)
+	ip := util.GetIPInfo()
+	if ip != nil {
+		util.INFO.Log("连接成功, 当前IP:", ip.IP)
+	}
 }
